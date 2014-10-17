8d3ca1e7f4cf6263e526689324a7549cda2598e5
Bart Massey <bart@cs.pdx.edu>
2014-10-16 18:08:39 -0700

    got rid of completely bogus and complex logic

--- qpdecode.hs
+++ qpdecode.hs
@@ -9,42 +9,19 @@ import Data.ByteString.Lazy as B hiding (getContents)
 import Data.Char (digitToInt, isHexDigit, ord)
 import Data.Word
 
-data Accum = Accum { code :: ByteString, remaining :: Int }
-
-clear :: Accum
-clear = Accum B.empty 0
-
-leadingOnes :: Int -> Int
-leadingOnes x
-    | x < 0x80 = 0
-    | otherwise = 1 + leadingOnes ((x - 0x80) * 2)
-
 byte :: Char -> Word8
 byte c = fromIntegral $ ord c
 
-decodeChars :: Accum -> String -> ByteString
-decodeChars _ "" = B.empty
-decodeChars accum ('=' : '\n' : cs) = decodeChars accum cs
-decodeChars accum ('=' : '\r' : '\n' : cs) = decodeChars accum cs
-decodeChars accum ('=' : d1 : d2 : cs) | isHexDigit d1 && isHexDigit d2 =
+decodeChars :: String -> ByteString
+decodeChars "" = B.empty
+decodeChars ('=' : '\n' : cs) = decodeChars cs
+decodeChars ('=' : '\r' : '\n' : cs) = decodeChars cs
+decodeChars ('=' : d1 : d2 : cs) | isHexDigit d1 && isHexDigit d2 =
   let
       thisDigit = 0x10 * digitToInt d1 + digitToInt d2
   in
-    if thisDigit < 0x80
-    then fromIntegral thisDigit `B.cons` decodeChars clear cs
-    else
-        let
-            allDigits = code accum `B.snoc` fromIntegral thisDigit
-            r = remaining accum
-        in
-          if thisDigit < 0xc0
-          then if r == 0
-               then allDigits `B.append` decodeChars clear cs
-               else decodeChars (Accum allDigits (r - 1)) cs
-          else decodeChars (Accum
-                            (B.singleton $ fromIntegral thisDigit)
-                            (leadingOnes thisDigit - 2)) cs
-decodeChars _ (c : cs) = byte c `B.cons` decodeChars clear cs
+    fromIntegral thisDigit `B.cons` decodeChars cs
+decodeChars (c : cs) = byte c `B.cons` decodeChars cs
 
 main :: IO ()
-main = getContents >>= B.putStr . decodeChars clear
+main = getContents >>= B.putStr . decodeChars
