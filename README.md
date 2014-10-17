176a17a8058774f1b2d07e329c4073f2ad64df7d
Bart Massey <bart@cs.pdx.edu>
2014-10-16 18:04:54 -0700

    quoted printable decoder in Haskell

---
+++ README.md
@@ -0,0 +1,11 @@
+# qpdecode - Quoted-Printable decoder in Haskell
+Copyright © 2014 Bart Massey
+
+This is a very simple quoted-printable (RFC 2045 sec 6.2)
+decoder in Haskell. It reads standard input and writes
+standard output. It has currently been benchmarked at about
+5MB/sec on a fast box. I have no idea how correct it is.
+
+This work is licensed under the "MIT License". Please see
+the file COPYING in this distribution for license terms.
+
---
+++ qpdecode.hs
@@ -0,0 +1,45 @@
+-- Decode quoted-printable text
+
+import Data.ByteString.Lazy as B hiding (getContents)
+import Data.Char (digitToInt, isHexDigit, ord)
+import Data.Word
+
+data Accum = Accum { code :: ByteString, remaining :: Int }
+
+clear :: Accum
+clear = Accum B.empty 0
+
+leadingOnes :: Int -> Int
+leadingOnes x
+    | x < 0x80 = 0
+    | otherwise = 1 + leadingOnes ((x - 0x80) * 2)
+
+byte :: Char -> Word8
+byte c = fromIntegral $ ord c
+
+decodeChars :: Accum -> String -> ByteString
+decodeChars _ "" = B.empty
+decodeChars accum ('=' : '\n' : cs) = decodeChars accum cs
+decodeChars accum ('=' : '\r' : '\n' : cs) = decodeChars accum cs
+decodeChars accum ('=' : d1 : d2 : cs) | isHexDigit d1 && isHexDigit d2 =
+  let
+      thisDigit = 0x10 * digitToInt d1 + digitToInt d2
+  in
+    if thisDigit < 0x80
+    then fromIntegral thisDigit `B.cons` decodeChars clear cs
+    else
+        let
+            allDigits = code accum `B.snoc` fromIntegral thisDigit
+            r = remaining accum
+        in
+          if thisDigit < 0xc0
+          then if r == 0
+               then allDigits `B.append` decodeChars clear cs
+               else decodeChars (Accum allDigits (r - 1)) cs
+          else decodeChars (Accum
+                            (B.singleton $ fromIntegral thisDigit)
+                            (leadingOnes thisDigit - 2)) cs
+decodeChars _ (c : cs) = byte c `B.cons` decodeChars clear cs
+
+main :: IO ()
+main = getContents >>= B.putStr . decodeChars clear
